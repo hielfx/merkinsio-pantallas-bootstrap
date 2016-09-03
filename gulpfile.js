880c8fae9441ec832ec75dd74eca754a4d71d067
Daniel Sánchez <dansanav@gmail.com>
2016-09-03 17:23:20 +0200

    ADD: Gulp, Bower and Project OS and name
    Created grunt file to inject bower dependencies into dashboard.html.
    Restructured the project and created package.json file to add project info (it's not completed yet).

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Daniel Sánchez on 03/09/2016.
+ */
+var gulp = require('gulp');
+var inject = require('gulp-inject');
+
+gulp.task('dashboard', function (cb) {
+    var target = gulp.src('dashboard.html');
+    var sources = gulp.src([
+        //JavaScript files
+        'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/bootstrap/dist/js/bootstrap.min.js',
+        'js/*.js',
+
+        // CSS files
+        'bower_components/bootstrap/dist/css/bootstrap.min.css',
+        'bower_components/bootstrap/dist/css/bootstrap.min.css',
+        'bower_components/bootstrap/dist/css/bootstrap-theme.min.css',
+        'css/*.css'
+
+    ], {read: false});
+
+    return target
+        .pipe(inject(sources, {addRootSlash: false}))
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('default', ['dashboard']);
