59d2d0f8b6fdd5cf4ee78a8793d34186687f08bc
Daniel Sánchez <dansanav@gmail.com>
2016-09-07 18:22:51 +0200

    ADD: Accordion status bar moving

--- js/accordion-panel.js
+++ js/accordion-panel.js
@@ -5,17 +5,17 @@
 $(document).ready(function () {
     $('a').tooltip({placement: 'top'});
 
+    /* Click function to expand and collapse changing the arrow*/
     $("#build-tab-toggle").click(function () {
-        $("#build-tab-collapse").slideToggle("slow");
+        $("#build-tab-collapse").slideToggle(400, resizeBuildStatus);
 
         $('#build-tab-toggle').toggleClass(function () {
-            if ($(this).is('.fa fa-chevron-down')) {
-                return '.fa fa-chevron-up';
+            if ($(this).is('.fa fa-chevron-circle-down')) {
+                return '.fa fa-chevron-circle-up';
             } else {
                 console.log('down');
-                return '.fa fa-chevron-down';
+                return '.fa fa-chevron-circle-down';
             }
-        })
+        });
     });
-
 });
