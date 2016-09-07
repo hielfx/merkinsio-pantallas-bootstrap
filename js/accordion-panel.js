4726731428f1436d08c09b73895a9462ec3d7175
Daniel Sánchez <dansanav@gmail.com>
2016-09-07 23:50:15 +0200

    FIX: Accordion on multiple panels
    The acordion works now when there are more panels than one (changed id selector to class selector)

--- js/accordion-panel.js
+++ js/accordion-panel.js
@@ -6,16 +6,29 @@ $(document).ready(function () {
     $('a').tooltip({placement: 'top'});
 
     /* Click function to expand and collapse changing the arrow*/
-    $("#build-tab-toggle").click(function () {
-        $("#build-tab-collapse").slideToggle(400, resizeBuildStatus);
+    $(".build-tab-toggle").click(function (e) {
+        /* Get the target element*/
+        var target = e.target;
 
-        $('#build-tab-toggle').toggleClass(function () {
-            if ($(this).is('.fa fa-chevron-circle-down')) {
-                return '.fa fa-chevron-circle-up';
-            } else {
-                console.log('down');
-                return '.fa fa-chevron-circle-down';
-            }
-        });
+        if (target === this) {
+
+            /* Get the common div for the toggle and collapse accordion*/
+            var parents = $($($(target).parent()[0]).parent()[0]).parent()[0];
+
+            /* Get the collapse element*/
+            var collapse = $(parents).find('.build-tab-collapse')[0];
+
+            /* Trigger the expand/collapse*/
+            $(collapse).slideToggle(400, resizeBuildStatus);
+
+            $(target).toggleClass(function () {
+                if ($(this).is('.fa fa-chevron-circle-down')) {
+                    return '.fa fa-chevron-circle-up';
+                } else {
+                    console.log('down');
+                    return '.fa fa-chevron-circle-down';
+                }
+            });
+        }
     });
 });
