e21f57b402f8a7bf640e767d106ff0018531fd5e
Daniel Sánchez <dansanav@gmail.com>
2016-09-08 19:34:29 +0200

    ADD: Modal form working test

---
+++ js/accordion-settings-modal.js
@@ -0,0 +1,88 @@
+/**
+ * Created by Daniel Sánchez on 08/09/2016.
+ */
+
+$(function () {
+    // $('div#settings-tab fieldset span img').each(function(index,element){
+    //     console.log(element);
+    //     $(element).click(function(e){
+    //         if(e.target === this){
+    //             $('#settings-modal').modal('show');
+    //         }
+    //     });
+    // });
+});
+
+$('#settings-modal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); //Modal trigger
+    var modal = this;
+
+    var id = $(button).attr('id');
+
+    switch (id) {
+        /* ----- Git ----- */
+        case 'modal-toggle-bitbucket':
+            $('#modal-title').html('Configuración de Bitbucket');
+            break;
+        case 'modal-toggle-github':
+            $('#modal-title').html('Configuración de GitHub');
+            break;
+        case 'modal-toggle-gitlab':
+            $('#modal-title').html('Configuración de GitLab');
+            break;
+        /* --------------- */
+
+        /* ----- Communication ----- */
+        case 'modal-toggle-slack':
+            $('#modal-title').html('Configuración de Slack');
+            break;
+        // case 'modal-toggle-jira':
+        //     break;
+        // case 'modal-toggle-trello':
+        //     break;
+        /* ------------------------- */
+
+        /* ----- Distribution ----- */
+        case 'modal-toggle-applivery':
+            $('#modal-title').html('Configuración de Applivery');
+            break;
+        case 'modal-toggle-crashlytics':
+            $('#modal-title').html('Configuración de Crashlytics');
+            $('#modal-form-crashlytics').addClass('modal-form-active');
+            $('#modal-form-crashlytics').removeClass('modal-form-disabled');
+            break;
+        case 'modal-toggle-testfairy':
+            $('#modal-title').html('Configuración de TestFairy');
+            break;
+        // case 'modal-toggle-hockeyapp':
+        //     break;
+        case 'modal-toggle-mail':
+            $('#modal-title').html('Configuración de Correo');
+            break;
+        /* ------------------------- */
+
+        /* ----- Test ----- */
+        case 'modal-toggle-apptize':
+            $('#modal-title').html('Configuración de Apptize');
+            break;
+        // case 'modal-toggle-aws-mobile':
+        //     break;
+        // case 'modal-toggle-appium':
+        //     break;
+        /* ---------------- */
+
+        /* ----- Default and not implemented yet ----- */
+        default:
+            $('#modal-title').html('Próximamente');
+            break;
+        /* --------------------------------------------*/
+    }
+});
+
+// Clear the modal on exit
+$('#settings-modal').on('hide.bs.modal', function (event) {
+    $('.modal-form-active').each(function (index, element) {
+        $(element).removeClass('modal-form-active');
+        $(element).addClass('modal-form-disabled');
+    });
+});
